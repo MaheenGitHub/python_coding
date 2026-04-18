96fea7a49ddae36057fc69ac9d8477f3ee94376a
Maheen Fatima <maheen19pgc@gmail.com>
2026-04-19 02:22:01 +0500

    quick recap of nested lists

---
+++ Lists/nestedLists.py
@@ -0,0 +1,17 @@
+num = [0,1,2,3,[40,50,60],4,5]
+print(len(num))
+
+print(num)
+print(num[3])
+
+print(num[4])
+print(num[4][0])
+
+print("slicing")
+print(num[3:])
+print(num[4][0:2])
+print(num[4][::2])
+
+list2 = [10,34,90,['maheen','ahmad','ali'],89]
+print(list2)
+print(list2[3][2])
