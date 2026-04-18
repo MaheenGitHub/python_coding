7fdf815e09ef28e43a0d7e6ce4e9af391a97385c
Maheen Fatima <maheen19pgc@gmail.com>
2026-04-18 18:17:44 +0500

    learned f strings

---
+++ f_string.py
@@ -0,0 +1,13 @@
+#formatted strings , easy adn faster , less error prone
+
+name = "Maheen"
+age = 23
+
+print("my name is", name , "and my age is" , age)
+
+print(f"My name is {name} and my age is {age}")
+
+#prefix f and then immediately after it, inverted commans and {variable}
+
+print("half my age is" , age/2)
+print(f"half my age is {age/2}")
